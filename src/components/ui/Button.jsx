938333899826accde31777f677e55d03cd422c31
jainayaan99-art <jainayaan99@gmail.com>
2025-11-19 18:51:56 +0800

    Add Button and Input components

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,4 @@
+// src/components/ui/Button.jsx
+export default function Button({ children, ...props }) {
+  return <button {...props}>{children}</button>
+}
---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,4 @@
+// src/components/ui/Input.jsx
+export default function Input(props) {
+  return <input {...props} />
+}
